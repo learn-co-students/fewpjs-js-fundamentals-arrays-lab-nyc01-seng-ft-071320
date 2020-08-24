125d3aac4f458cc750c1691d2d695de697ec5fd6
Alex <alex.beciana@gmail.com>
2020-08-23 22:17:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution here!
+
+
+const prepend = ["Milo", "Otis", "Garfield"]
+const append = ["Milo", "Otis", "Garfield"]
+const removeLast = ["Milo", "Otis", "Garfield"]
+const removeFirst = ["Milo", "Otis", "Garfield"]
+
+append.push("Odie")
+
+removeLast.pop()
+
+prepend.unshift("Odie")
+
+removeFirst.shift()
+
+removeFirst
+
+prepend
+
+removeLast
+
+append
